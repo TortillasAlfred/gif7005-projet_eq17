b0a2641aa96905bb75b45adcb58b69999ae1d668
Roxane Debruyker <roxane.debruyker.1@ulaval.ca>
2018-11-23 11:41:34 -0500

    Add vectorizer - work but dirty

--- loading/wordVectorizer.py
+++ loading/wordVectorizer.py
@@ -23,15 +23,27 @@ class WordVectorizer(object):
         return wv
 
     def generate_avg_word_vectors(self, queries):
-        wv = {}
-        for query in queries:
-            doc = self.nlp(str(query))
-            exp = []
+        queries = np.array(queries)
+        wv = np.zeros([queries.size, 300])
+        for i in range(queries.size):
+            doc = self.nlp(str(queries[i]))
+            exp = list()
             for token in doc:
                 if token.has_vector:
                     exp.append(token.vector)
-            wv[query] = np.mean(exp, axis=0)
+            if not exp:
+                wv[i] = np.zeros(300)
+            else:
+                wv[i] = np.mean(exp, axis=0)
         return wv
+
+    def fit_transform(self, data_train, *data):
+        transformed_data = list()
+        transformed_data.append(np.array(list(self.generate_avg_word_vectors(data_train))))
+        for d in data:
+            transformed_data.append(np.array(list(self.generate_avg_word_vectors(d))))
+        return transformed_data
+
 
 if __name__=="__main__":
 
