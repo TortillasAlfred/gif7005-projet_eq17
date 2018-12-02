f43704e27d014253bb36c7b1c5f4d43d5df7d916
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2018-12-02 08:16:24 -0500

    Typo in coveo_scorer

--- scorers/coveo_scorer.py
+++ scorers/coveo_scorer.py
@@ -6,8 +6,8 @@ def coveo_score(y_true, y_pred):
 
     intersection = np.asarray([np.intersect1d(y_true[i], y_pred[i]) for i in range(len(y_true))])
     correct = np.asarray([len(col) > 0 for col in intersection])
-    possible_to_predict = np.asarray([len(col) == 0 for col in y_true])
-    correct = np.ma.masked_array(correct, mask=possible_to_predict)
+    impossible_to_predict = np.asarray([len(col) == 0 for col in y_true])
+    correct = np.ma.masked_array(correct, mask=impossible_to_predict)
 
     return np.mean(correct)
     
