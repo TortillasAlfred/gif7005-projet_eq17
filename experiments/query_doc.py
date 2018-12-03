6803696e1e53c190ead062275ea00ea03789d52e
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2018-12-03 11:07:56 -0500

    Added train on whole dataset

--- experiments/query_doc.py
+++ experiments/query_doc.py
@@ -5,7 +5,7 @@ from loading.oneHotEncoder import OneHotEncoder
 from wrappers.qd_regression_wrapper import QueryDocRegressionWrapper
 from wrappers.regression_wrapper import RegressionWrapper
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression, SGDRegressor
 
 class PoC:
     def __init__(self, load_from_numpy):
@@ -18,17 +18,17 @@ class PoC:
                                     load_from_numpy=load_from_numpy, filter_no_clicks=False)
 
     def run_experiment(self):
-        self.run_normal_wrapped()
-        self.run_qd_wrapped()
+        # self.run_normal_wrapped()
+        self.run_qd_wrapped_all_dataset()
 
-    def run_qd_wrapped(self):
-        print("**** QD-WRAPPED LIN REG ****")
+    def run_qd_wrapped_all_dataset(self):
+        print("**** QD-WRAPPED SGD REG ALL DATASET ****")
         self.loader_wv.load_transform_data()
         X_train, X_valid, y_train, y_valid, all_docs = self.loader_wv.load_all_from_numpy("X_train", "X_valid",
                                                                                           "y_train", "y_valid",
                                                                                           "all_docs")
 
-        reg = QueryDocRegressionWrapper(LinearRegression(), all_docs, proportion_neg_examples=1)
+        reg = QueryDocRegressionWrapper(SGDRegressor(verbose=1), all_docs, proportion_neg_examples=-1)
         reg.fit(X_train, y_train)
         print("Coveo score on train : {}".format(reg.score(X_train, y_train)))
         print("Coveo score on valid : {}".format(reg.score(X_valid, y_valid)))
