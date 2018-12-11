896d5c5ba9d738d7cc74e6cfb796172b19e5dbaa
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2018-12-11 12:00:56 -0500

    Bdl partial fit (#13)
    * Batch data loader works, but generates random pairs each time.
    
    * Random pairs are now loaded from a file by default in BatchDataLoader.
    
    * Added __ before private method names.
    
    * Took slicing logic out of QueryDocRegressionWrapper. Experiment does not work for now, random_pairs file is still missing.
    
    * Added random_pairs file. QueryDocBatchDataLoader should work.
    
    * Integer slices and rammable batch size
    
    * Save y in memory in BatchDataLoader for speed
    
    * BatchDataLoader on filtered dataset
    
    * Add random_pairs_filtered numpy array
    
    * Add class weights to BatchDataLoader
    
    * Added class weights and fixed experiment on QD

--- __main__.py
+++ __main__.py
@@ -3,9 +3,9 @@ from experiments.LR import LR
 from experiments.n_predicted_sample import GlobalExperiment
 
 if __name__ == "__main__":
-    # exp = PoC(load_from_numpy=False)
+    exp = PoC(load_from_numpy=False)
     # exp = LR(load_from_numpy=True)
-    exp = GlobalExperiment(load_from_numpy=False)
+    # exp = GlobalExperiment(load_from_numpy=False)
     # exp.run_experiment_LR()
-    exp.run_experiment_LR_balanced()
-    # exp.run_experiment()
+    # exp.run_experiment_LR_balanced()
+    exp.run_experiment()
