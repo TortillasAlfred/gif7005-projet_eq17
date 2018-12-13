aadba1da710e4651e5411f054d6450c5fb2ba383
Roxane Debruyker <roxane.debruyker.1@ulaval.ca>
2018-12-13 09:32:03 -0500

    Seperate in different classes the WordVectorizer

--- loading/wordVectorizer.py
+++ loading/wordVectorizer.py
@@ -2,7 +2,6 @@ import spacy
 import numpy as np
 
 from loading.bagOfWordsVectorizer import BagOfWordsVectorizer
-
 from loading.oneHotEncoder import OneHotEncoder
 from loading.dataLoader import DataLoader
 
@@ -13,7 +12,27 @@ class WordVectorizer(object):
     def __init__(self):
         self.nlp = spacy.load("en_vectors_web_lg")
 
+    def generate_matrix_word_vectors(self, queries):
+        '''
+        :param queries: iterator of queries in NL
+        :return: matrix with (nb_queries x MAX_QUERY_LENGTH x vector size(300) ) size
+        '''
+        queries = np.array(queries)
+        wv = np.zeros((queries.size, MAX_QUERY_LENGTH, 300))
+        for i in range(queries.size):
+            doc = self.nlp(str(queries[i]))
+            j = 0
+            for token in doc:
+                if token.has_vector and j < MAX_QUERY_LENGTH:
+                    wv[i, j] = token.vector
+                j += 1
+        return wv
+
     def generate_dict_word_vectors(self, queries):
+        '''
+        :param queries: iterator of queries in NL
+        :return: dictionary with query as key and list of word embeddings as value
+        '''
         wv = {}
         for query in queries:
             doc = self.nlp(str(query))
@@ -24,47 +43,83 @@ class WordVectorizer(object):
             wv[query] = exp
         return wv
 
-    def generate_matrix_word_vectors(self, queries):
-        queries = np.array(queries)
-        wv = np.zeros((queries.size, MAX_QUERY_LENGTH, 300))
-        for i in range(queries.size):
-            doc = self.nlp(str(queries[i]))
-            j=0
-            for token in doc:
-                if token.has_vector and j < MAX_QUERY_LENGTH:
-                    wv[i, j] = token.vector
-                j += 1
+    def generate_dict_sentence_vectors_spacy(self, queries):
+        '''
+        :param queries: iterator of queries in NL
+        :return: dictionary with query as key and sentence embedding as value (made by spacy)
+        '''
+
+        wv = {}
+        for query in queries:
+            doc = self.nlp(str(query))
+            wv[query] = doc.vector
         return wv
 
-    def generate_sentence_vector(self, queries):
-        queries = np.array(queries)
-        wv = np.zeros((queries.size, 300))
-        for i in range(queries.size):
-            doc = self.nlp(str(queries[i]))
-            wv[i] = doc.vector
-        return wv
+    def generate_dict_sentence_vectors_avghandmade(self, queries):
+        '''
+        :param queries: iterator of queries in NL
+        :return: dictionary with query as key and sentence embedding as value (with handmade mean of spacy word vectors)
+        '''
 
-    def generate_avg_word_vectors(self, queries):
-        queries = np.array(queries)
-        wv = np.zeros([queries.size, 300])
-        for i in range(queries.size):
-            doc = self.nlp(str(queries[i]))
+        wv = {}
+        for query in queries:
+            doc = self.nlp(str(query))
             exp = list()
             for token in doc:
                 if token.has_vector:
                     exp.append(token.vector)
             if not exp:
-                wv[i] = np.zeros(300)
+                wv[query] = np.zeros(300)
             else:
-                wv[i] = np.mean(exp, axis=0)
+                wv[query] = np.mean(exp, axis=0)
         return wv
 
+
+class MatrixWordVectorizer(WordVectorizer):
+    def __init__(self):
+        super(MatrixWordVectorizer, self).__init__()
+
     def fit_transform(self, data_train, *data):
-        self.nlp = spacy.load("en_vectors_web_lg")
         transformed_data = list()
-        transformed_data.append(np.array(list(self.generate_avg_word_vectors(data_train))))
+        transformed_data.append(np.array(list(self.generate_matrix_word_vectors(data_train))))
         for d in data:
-            transformed_data.append(np.array(list(self.generate_avg_word_vectors(d))))
+            transformed_data.append(np.array(list(self.generate_matrix_word_vectors(d))))
+        return transformed_data
+
+
+class DictWordVectorizer(WordVectorizer):
+    def __init__(self):
+        super(DictWordVectorizer, self).__init__()
+
+    def fit_transform(self, data_train, *data):
+        transformed_data = list()
+        transformed_data.append(np.array(list(self.generate_dict_word_vectors(data_train))))
+        for d in data:
+            transformed_data.append(np.array(list(self.generate_dict_word_vectors(d))))
+        return transformed_data
+
+
+class DictSentenceVectorizerSpacy(WordVectorizer):
+    def __init__(self):
+        super(DictSentenceVectorizerSpacy, self).__init__()
+
+    def fit_transform(self, data_train, *data):
+        transformed_data = list()
+        transformed_data.append(np.array(list(self.generate_dict_sentence_vectors_spacy(data_train))))
+        for d in data:
+            transformed_data.append(np.array(list(self.generate_dict_sentence_vectors_spacy(d))))
+        return transformed_data
+
+
+class DictSentenceVectorizerHM(WordVectorizer):
+    def __init__(self):
+        super(WordVectorizer, self).__init__()
+
+    def fit_transform(self, data_train, *data):
+        transformed_data = list()
+        transformed_data.append(np.array(list(self.generate_dict_sentence_vectors_avghandmade(data_train))))
+        for d in data:
+            transformed_data.append(np.array(list(self.generate_dict_sentence_vectors_avghandmade(d))))
         return transformed_data
 
 
@@ -80,7 +135,15 @@ if __name__=="__main__":
 
     loader.load_transform_data()
     searches_train = loader.load_all_from_pickle("searches_train")
-    WV = WordVectorizer()
-    test = WV.generate_matrix_word_vectors(searches_train.query_expression.values)
-    test2 = WV.generate_sentence_vector(searches_train.query_expression.values)
-    print("test")
+
+    test1 = MatrixWordVectorizer()
+    test1.fit_transform(searches_train.query_expression.values)
+
+    test2 = DictWordVectorizer()
+    test2.fit_transform(searches_train.query_expression.values)
+
+    test3 = DictSentenceVectorizerSpacy()
+    test3.fit_transform(searches_train.query_expression.values)
+
+    test4 = DictSentenceVectorizerHM()
+    test4.fit_transform(searches_train.query_expression.values)
