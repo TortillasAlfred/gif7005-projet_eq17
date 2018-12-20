37336f2b6ff54f2dca51bfae081627e0dff5213a
Roxane Debruyker <roxane.debruyker.1@ulaval.ca>
2018-12-20 14:13:15 -0500

    change for adapting with bow, works really better

--- experiments/knn.py
+++ experiments/knn.py
@@ -4,26 +4,44 @@ from loading.wordVectorizer import *
 from loading.dataLoader import DataLoader
 from loading.oneHotEncoder import OneHotEncoder
 
+from loading.bagOfWordsVectorizer import BagOfWordsVectorizer
 
-vectWV = DictSentenceVectorizerSpacy()
+
+vectWV = BagOfWordsVectorizer()
 enc = OneHotEncoder()
 loader = DataLoader(vectorizer=vectWV, one_hot_encoder=enc,
                                     search_features=DataLoader.default_search_features,
                                     click_features=DataLoader.default_click_features,
-                                    data_folder_path="../data/", numpy_folder_path="../data/qd_wv/",
+                                    data_folder_path="../data/", numpy_folder_path="../data/bow_oh_filtered/",
                                     load_from_numpy=True, filter_no_clicks=False)
 
 loader.load_transform_data()
-training_data = loader.load_all_from_numpy("test_train")
-validation_data = loader.load_all_from_numpy("test_valid")
+X_train = loader.load_all_from_numpy("X_train")
+X_valid = loader.load_all_from_numpy("X_valid")
+y_train = loader.load_all_from_numpy("y_train")
+y_valid = loader.load_all_from_numpy("y_valid")
 
-X_train = training_data[:,0]
+for i in range(len(y_train)):
+    while len(y_train[i]) > 1:
+        X_train = np.append(X_train, [X_train[i]], axis=0)
+        y_train = np.append(y_train, y_train[i][0])
+        y_train[i] = np.delete(y_train[i], 0)
+y_train = np.array(list(map(int, y_train)))
+
+for i in range(len(y_valid)):
+    while len(y_valid[i]) > 1:
+        X_valid = np.append(X_valid, [X_valid[i]], axis=0)
+        y_valid = np.append(y_valid, y_valid[i][0])
+        y_valid[i] = np.delete(y_valid[i], 0)
+y_valid = np.array(list(map(int, y_valid)))
+
+'''X_train = training_data[:,0]
 y_train = (np.sum(training_data[:, 1], axis=1)*1000)
 y_train = list(map(int, y_train))
 
 X_valid = validation_data[:,0]
 y_valid = np.sum(validation_data[:,1], axis=1)*1000
-y_valid = list(map(int, y_valid))
+y_valid = list(map(int, y_valid))'''
 
 for n in [1,3,10,30]:
     clf = KNeighborsClassifier(n_neighbors=n, weights='distance', algorithm='auto')
