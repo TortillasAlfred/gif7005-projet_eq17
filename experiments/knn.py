33fab6476e08f16807490993670dc3261ca007ff
Roxane Debruyker <roxane.debruyker.1@ulaval.ca>
2018-12-20 18:19:43 -0500

    clean but dunno if it works yet

--- experiments/knn.py
+++ experiments/knn.py
@@ -1,63 +1,58 @@
+import numpy as np
+
 from sklearn.neighbors import KNeighborsClassifier
 
-from loading.wordVectorizer import *
 from loading.dataLoader import DataLoader
 from loading.oneHotEncoder import OneHotEncoder
-
 from loading.bagOfWordsVectorizer import BagOfWordsVectorizer
 
+from wrappers.knn_wrapper import KNNWrapper
 
-vectWV = BagOfWordsVectorizer()
-enc = OneHotEncoder()
-loader = DataLoader(vectorizer=vectWV, one_hot_encoder=enc,
-                                    search_features=DataLoader.default_search_features,
-                                    click_features=DataLoader.default_click_features,
-                                    data_folder_path="../data/", numpy_folder_path="../data/bow_oh_filtered/",
-                                    load_from_numpy=True, filter_no_clicks=False)
+class KNN:
+    def __init__(self):
 
-loader.load_transform_data()
-X_train = loader.load_all_from_numpy("X_train")
-X_valid = loader.load_all_from_numpy("X_valid")
-y_train = loader.load_all_from_numpy("y_train")
-y_valid = loader.load_all_from_numpy("y_valid")
+        vectWV = BagOfWordsVectorizer()
+        enc = OneHotEncoder()
+        self.loader = DataLoader(vectorizer=vectWV, one_hot_encoder=enc,
+                                            search_features=DataLoader.default_search_features,
+                                            click_features=DataLoader.default_click_features,
+                                            data_folder_path="../data/", numpy_folder_path="../data/bow_oh_filtered/",
+                                            load_from_numpy=True, filter_no_clicks=False)
 
-for i in range(len(y_train)):
-    while len(y_train[i]) > 1:
-        X_train = np.append(X_train, [X_train[i]], axis=0)
-        y_train = np.append(y_train, y_train[i][0])
-        y_train[i] = np.delete(y_train[i], 0)
-y_train = np.array(list(map(int, y_train)))
+    def preprocess_data(self):
+        X_train, X_valid, _, y_train, y_valid, _, _ = self.loader.load_transform_data()
 
-for i in range(len(y_valid)):
-    while len(y_valid[i]) > 1:
-        X_valid = np.append(X_valid, [X_valid[i]], axis=0)
-        y_valid = np.append(y_valid, y_valid[i][0])
-        y_valid[i] = np.delete(y_valid[i], 0)
-y_valid = np.array(list(map(int, y_valid)))
+        for i in range(len(y_train)):
+            while len(y_train[i]) > 1:
+                X_train = np.append(X_train, [X_train[i]], axis=0)
+                y_train = np.append(y_train, y_train[i][0])
+                y_train[i] = np.delete(y_train[i], 0)
+        y_train = np.array(list(map(int, y_train)))
 
-'''X_train = training_data[:,0]
-y_train = (np.sum(training_data[:, 1], axis=1)*1000)
-y_train = list(map(int, y_train))
+        for i in range(len(y_valid)):
+            while len(y_valid[i]) > 1:
+                X_valid = np.append(X_valid, [X_valid[i]], axis=0)
+                y_valid = np.append(y_valid, y_valid[i][0])
+                y_valid[i] = np.delete(y_valid[i], 0)
+        y_valid = np.array(list(map(int, y_valid)))
 
-X_valid = validation_data[:,0]
-y_valid = np.sum(validation_data[:,1], axis=1)*1000
-y_valid = list(map(int, y_valid))'''
+        return X_train, X_valid, y_train, y_valid
 
-for n in [1,3,10,30]:
-    clf = KNeighborsClassifier(n_neighbors=n, weights='distance', algorithm='auto')
-    clf.fit(X_train, y_train)
+    def run_filtered(self):
+        print("**** FILTERED EXP ****")
+        X_train, X_valid, y_train, y_valid = self.preprocess_data()
 
-    predicts = clf.predict(X_valid)
-    kneighbors = clf.kneighbors(X_valid, 4, return_distance=False)
+        knn = KNNWrapper(n_neighbors=17, weights='distance', algorithm='auto')
+        knn.fit(X_train, y_train)
 
-    predicted_neighbors = np.zeros((kneighbors.shape[0], kneighbors.shape[1]+1))
+        print("Coveo score on train : {}".format(knn.score(X_train, y_train)))
+        print("Coveo score on valid : {}".format(knn.score(X_valid, y_valid)))
 
-    good = 0
-    for i in range(len(kneighbors)):
-        predicted_neighbors[i][0] = predicts[i]
-        for j in range(len(kneighbors[i])):
-            predicted_neighbors[i][j+1] = y_train[kneighbors[i,j]]
-        if y_valid[i] in predicted_neighbors[i]:
-            good += 1
 
-    print(good)
+if __name__ == "__main__":
+    test = KNN()
+    test.run_filtered()
+
+
+
+
---
+++ wrappers/knn_wrapper.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from scorers.coveo_scorer import coveo_score
+
+
+class KNNWrapper:
+    def __init__(self, n_neighbors, weights, algorithm):
+        self.clf = KNeighborsClassifier(n_neighbors, weights, algorithm)
+
+    def fit(self, X, y):
+        self.X, self.y = X, y
+        self.clf.fit(X, y)
+
+    def predict(self, X, n_predicted_per_sample=5):
+        y_predict = self.clf.predict(X)
+        k_neighbors = self.clf.kneighbors(X, n_predicted_per_sample-1, return_distance=False)
+        y_predict_raw = np.zeros((k_neighbors.shape[0], k_neighbors.shape[1] + 1))
+
+        for i in range(len(y_predict)):
+            y_predict_raw[i][0] = y_predict[i]
+            for j in range(n_predicted_per_sample-1):
+                y_predict_raw[i][j + 1] = self.y[k_neighbors[i, j]]
+
+        return y_predict_raw
+
+    def score(self, X, y_true):
+        y_pred = self.predict(X)
+        return coveo_score(y_true, y_pred)
